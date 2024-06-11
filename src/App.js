fe96e90e04ccc83c3f8fadd5caae02e014e4bc1a
Minh Hieu <vandangthi258@gmail.com>
2024-06-11 23:09:17 +0700

    First commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,59 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import axios from 'axios';
 
 function App() {
+  const [file, setFile] = useState(null);
+  const [predictions, setPredictions] = useState([]);
+
+  const handleFileChange = (event) => {
+    setFile(event.target.files[0]);
+  };
+
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+    const formData = new FormData();
+    formData.append('file', file);
+
+    try {
+      const response = await axios.post('http://127.0.0.1:5000/predict', formData, {
+        headers: {
+          'Content-Type': 'multipart/form-data',
+        },
+      });
+      setPredictions(response.data);
+    } catch (error) {
+      console.error('Error uploading file:', error);
+    }
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Upload CSV and Get Predictions</h1>
+      <form onSubmit={handleSubmit}>
+        <input type="file" accept=".csv" onChange={handleFileChange} />
+        <button type="submit">Upload</button>
+      </form>
+      {predictions.length > 0 && (
+        <div style={{width: '100%' ,display: 'flex', justifyContent: 'center'}}>
+          <h2>Predictions</h2>
+          <table>
+            <thead>
+              <tr>
+                <th>Store</th>
+                <th>Weeksly_Sales</th>
+              </tr>
+            </thead>
+            <tbody>
+              {predictions.map((prediction, index) => (
+                <tr key={index}>
+                  <td>{prediction.Store}</td>
+                  <td>{prediction.Prediction}</td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+        </div>
+      )}
     </div>
   );
 }
